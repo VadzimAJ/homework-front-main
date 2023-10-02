2e7bc3f92072967ab2e1811de9137958f140cf0f
VadzimAJ <vadzim.anishchanka.j@gmail.com>
2023-10-02 18:26:31 +0300

    feat: complite HW2 task

--- src/HW/2/CurrentUser.tsx
+++ src/HW/2/CurrentUser.tsx
@@ -1,7 +1,9 @@
 import { UserType } from './HW2';
+import {UsersObjectType} from './HW2';
 
 type CurrentUserPropsType = {
-  user: any // пропиши типизацию
+  user: UserType // пропиши типизацию
+  filterUsers: (users: UsersObjectType) => void;
 };
 
 export const CurrentUser = (props: CurrentUserPropsType) => {
